3bccadfbeb6c2248caed23f508a9491699815bab
AlxInc <108522772+AlxInc@users.noreply.github.com>
2023-02-24 14:43:23 +1030

    a few mistakes on module 4

--- ssg/content.py
+++ ssg/content.py
@@ -3,17 +3,17 @@ from yaml import load, FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
+    __delimeter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimeter, re.MULTILINE)
     @classmethod
     def load(self, cls, string):
-        _, fm, content = self.__regex.split(string, 2)
-        load(fm, Loader=FullLoader)
-        return cls(self.metadata, content)
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
 
     def __init__(self, metadata, content):
-        data = metadata
-        self.data = {content : "content"}
+        self.data = metadata
+        self.data["content"] = content
 
     @property
     def body(self):
@@ -21,20 +21,17 @@ class Content(Mapping):
 
     @property
     def type(self):
-        if self.data.keys(type):
-            return self.data["type"]
-        else:
-            return None
+        return self.data["type"] if "type" in self.data else None
 
     @type.setter
-    def type(self, data):
-        data = self._data = ["type"]
+    def type(self, type):
+        self.data["type"] = type
 
     def __getitem__(self, key):
         return self.data[key]
 
     def __iter__(self):
-        return iter(self.data)
+        self.data.__iter__()
 
     def __len__(self):
         return len(self.data)
@@ -42,7 +39,7 @@ class Content(Mapping):
     def __repr__(self):
         data = {}
         for key, value in self.data.items():
-            if key == "content":
+            if key != "content":
                 data[key] = value
         return str(data)
 
