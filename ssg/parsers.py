3bf077e8a4f19cdab3be190357c73387a25672b0
AlxInc <108522772+AlxInc@users.noreply.github.com>
2023-03-09 12:01:15 +1030

    missed a few questions

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -42,13 +42,13 @@ class MarkdownParser(Parser):
     def parse(self, path: Path, source: Path, dest: Path) -> Path:
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(path, dest)
+        self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 class ReStructuredTextParser(Parser):
-
+    extensions = [".rst"]
     def parse(self, path: Path, source: Path, dest: Path) -> Path:
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
-        self.write(html["html_body"], dest)
+        self.write(html["html_body"], dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
